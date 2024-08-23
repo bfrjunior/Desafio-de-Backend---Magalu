341b9e742fceac34c0d3a5be364c80936473507e
Fernando Rocha <beneditofernandor@gmail.com>
2024-08-23 00:50:37 -0300

    feat: initial commit

---
+++ src/test/java/br/com/bfrjunior/magalums/MagalumsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bfrjunior.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
